1633ccf50ce9d9eed3b1c05ae99abfb8bd5bb6ba
matix18 <matix18@github.com>
2020-10-15 16:52:05 +0000

    first commit

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -12,149 +12,57 @@ import {
   ImageBackground,
   TextInput,
   ActivityIndicator,
-  Alert,
-  Title,
 } from "react-native";
-import axios from 'axios';
-import getImageForWeather from "../api/getImageForWeather";
-import { fetchLocationId, fetchWeather } from "../api/api";
-import SearchInput from './SearchInput';
 
-export default class SearchScreen extends Component {
-  constructor(){
+export default class SearchInput extends Component {
+  constructor() {
     super()
     this.state = {
-      info: {
-        name:"loading",
-        temp:"loading..",
-        humidity:"",
-        desc:"loading..",
-        icon:"loading..",
-        country:"loading",
-      },
-    };
-  }
-
-  changeLocation = (e) => {
-    this.getWeather(e)
-  }
-
-  getWeather = (e) => {
-    if(!e){
-      this.state = {
-        info:{
-          name:'Dakar'
-        }
-      }
-    } else {
-      this.state = {
-        info: {
-          name: e
-        }
-      }
+      text : ''
     }
+  }
 
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${this.state.info.name}&appid=5e49b81ebcd594016605bdcda2e43092`
-      ).then(
-        response => response.json()
-      ).then(
-        data => {
-          this.setState({
-            info:{
-              name:data.name,
-              temp:data.main.temp,
-              humidity:data.main.humidity,
-              desc:data.weather[0].description,
-              icon:data.weather[0].icon,
-              country:data.sys.country,
-            },
-            loading:true,
-          })
-          // console.log(this.state.info)
-        }).catch(err => {
-          Alert.alert("Erreur" + err.message + "Connexion failed!",[{text:"ok"}])
-        })
+  handleText = (e) => {
+    this.setState({
+      text: e
+    })
   }
-  componentDidMount() {
-    this.getWeather();
+
+  handleSubmit = () => {
+    if (!this.state.text) return;
+    this.props.onSubmit(this.state.text)
+    this.setState({
+      text : ''
+    })  
   }
-  
+
   render() {
-    const { loading } = this.state
-    setTimeout(() => this.setState({ loading: false }), 5000)
+      const { handleText } = this.props;
+      const { text } = this.state;
+      const { handleSubmit } = this.props;
       return (
-            <ImageBackground
-              style={styles.bgImage}
-              source={require("../assets/sleet.png")}
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-            {!loading && (
-                <View>
-                  <View style={styles.container}>
-                    <Text style={styles.climate}>{this.state.info.country}</Text>
-                    <Text style={styles.text}>{this.state.info.name}</Text>
-                    <Text style={styles.climate}>{this.state.info.temp}°C</Text>
-                    <Text style={styles.climate}>{this.state.info.desc}</Text>
-                    <Image style={{ width: 100, height: 100, alignContent:'center' }} source={{ uri:'http://openweathermap.org/img/wn/'+this.state.info.icon+".png"}} />
-                    <Text style={styles.climate}>{this.state.info.humidity}</Text>
-                  </View>
-                </View>
-            )}
-            {loading && (
-              
-              <ActivityIndicator />
-            )}
-              <SearchInput onSubmit={this.changeLocation} />
-          </ImageBackground>
-          );
-      }
+        <View>
+          <TextInput
+            style={styles.search}
+            placeholder="rechercher une ville"
+            onChangeText={this.handleText} // si le text change et que le fils gère ses données, pas le parent
+            onSubmitEditing={this.handleSubmit} // lorsqu'on tape sur le bouton Entrée
+            value={text}
+          />
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    fontWeight:'bold',
-    fontSize:'40px',
-  },
-  button: {
-    backgroundColor: "steelblue",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    color: "white",
-    fontSize: 42,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: '40px',
-  },
-  climate:{
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: '20px',
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
-  row: {
-    padding: 15,
-    marginBottom: 5,
-    backgroundColor: "skyblue",
-  },
-  layout: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0.05)",
-  },
-  box: {
-    padding: 25,
-    backgroundColor: "steelblue",
-    margin: 5,
+  search: {
+    backgroundColor: "#fff",
+    width: "450px",
+    height: "30px",
+    textTransform: "uppercase",
+    textAlign: "center",
+    margin: "auto",
+    marginBottom: "-10%",
+    borderRadius: '50px',
   },
 });
